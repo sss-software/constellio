5d60e3541f6fc10696d7acfeea7e5f6ce07bf60a
ericgiguere <eric.giguere@constellio.com>
2019-02-15 14:43:15 -0500

    Fix project setup for Eclipse

--- model/src/com/constellio/model/conf/FoldersLocator.java
+++ model/src/com/constellio/model/conf/FoldersLocator.java
@@ -48,7 +48,8 @@ public class FoldersLocator {
 		} else {
 			File classFolder = new File(fullPath);
 
-			if (classFolder.getParentFile().getParentFile().getParentFile().getParentFile().getName().equals("bin")) {
+			if (classFolder.getParentFile().getParentFile().getParentFile().getParentFile().getParentFile().getName()
+					.equals("bin")) {
 				finalPath = classFolder.getParentFile().getParentFile().getParentFile().getParentFile().getParentFile()
 						.getParentFile().getAbsoluteFile();
 			} else {
